2bb4641674d05d3eb69f238efd65a572acf160d2
Surayyo <124129374+surayyo-kadirova@users.noreply.github.com>
2024-07-17 15:29:05 +0300

    Add files via upload

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,53 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : Controller
+    {
+        private IHotelService _hotelService;
+
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService; 
+        }
+
+        [HttpGet]
+        public List<Hotel> Get()
+        {
+            return _hotelService.GetAllHotels();
+        }
+
+        [HttpGet("{Id}")]
+        public Hotel Get(int Id)
+        {
+            return _hotelService.GetHotelById(Id);
+        }
+
+        [HttpPost]
+        public Hotel Post([FromBody]Hotel hotel)
+        {
+            return _hotelService.CreateHotel(hotel);
+        }
+
+        [HttpPut]
+        public Hotel Put([FromBody]Hotel hotel)
+        {
+            return _hotelService.UpdateHotel(hotel);
+        }
+
+        [HttpDelete("{Id}")]
+        public void Delete(int Id)
+        {
+            _hotelService.DeleteHotel(Id);
+        }
+
+      
+    }
+}
+
---
+++ HotelFinder.API/Program.cs
@@ -0,0 +1,28 @@
+﻿// In Program.cs for .NET 6 and later
+using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.DatabaseContext.Abstract;
+using HotelFinder.DatabaseContext.Concrete;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+
+// Register IHotelService with its implementation
+builder.Services.AddSingleton<IHotelService, HotelManager>();
+builder.Services.AddSingleton<IHotelRepository, HotelRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
